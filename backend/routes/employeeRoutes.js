834f1452ca5b6d5e74eb11879cad274cbda202c4
keshav303 <keshavkaushik658@gmail.com>
2026-06-27 17:14:07 +0530

    Add employee routes for CRUD operations

---
+++ backend/routes/employeeRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getAllEmployees,
+  getEmployeeById,
+  addEmployee,
+  updateEmployee,
+  deleteEmployee,
+} = require("../controllers/employeeController");
+
+router.get("/", getAllEmployees);
+router.get("/:id", getEmployeeById);
+router.post("/", addEmployee);
+router.put("/:id", updateEmployee);
+router.delete("/:id", deleteEmployee);
+
+module.exports = router; 
